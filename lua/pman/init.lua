a423a2a31257ff7d7edde3a6cf59980fd50feea9
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-16 17:02:44 +0200

    ADD6

--- lua/pman/init.lua
+++ lua/pman/init.lua
@@ -1,28 +1,39 @@
 local M = {}
 
 local config = {
-  header = {},
-  projects = {},
+  header = {
+    "  ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄   ▄▄  ",
+    " █  █  █ █       █       █  █ █  █   █  █▄█  █ ",
+    " █   █▄█ █    ▄▄▄█   ▄   █  █▄█  █   █       █ ",
+    " █       █   █▄▄▄█  █ █  █       █   █       █ ",
+    " █  ▄    █    ▄▄▄█  █▄█  █       █   █       █ ",
+    " █ █ █   █   █▄▄▄█       ██     ██   █ ██▄██ █ ",
+    " █▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄█▄█   █▄█ ",
+  },
+  projects = {
+    ["p1"] = "/path/to/project1",
+    ["p2"] = "/path/to/project2",
+    ["p3"] = "/path/to/project3",
+  },
 }
 
 local selected_index = 1
 
 function M.setup(user_config)
-  config = vim.tbl_deep_extend("force", config, user_config or {})
+  -- config = vim.tbl_deep_extend("force", config, user_config or {})
   
   -- Register command to switch between projects
   vim.api.nvim_create_user_command(
     "Pman",
     function(opts)
-      print()
-
       local project_name = opts.args[0]
 
-      print(project_name .. " : " .. config.projects[project_name])
+      local path = config.projects[project_name]
+
+      print(project_name .. " : " .. path)
     end,
     { nargs = 1, desc = "Switch to project" }
   )
-
 end
 
 return M
