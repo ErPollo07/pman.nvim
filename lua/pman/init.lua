25fbe1a6bd637330a3818d3d4028707a0379087a
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2025-04-16 16:49:33 +0200

    ADD2

--- lua/pman/init.lua
+++ lua/pman/init.lua
@@ -14,7 +14,7 @@ function M.setup(user_config)
   vim.api.nvim_create_user_command(
     "Pman",
     function(opts)
-      print(string.format("%s : %s", config.projects[project]))
+      print(string.format("%s : %s", config.projects[opts.args[1]]))
     end,
     { nargs = 1, desc = "Switch to project" }
   )
